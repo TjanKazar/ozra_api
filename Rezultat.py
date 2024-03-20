14db881e66f80a6d59417bcd822146741fced56a
TjanKazar <tjan.kazar3@gmail.com>
2024-03-20 13:09:33 +0100

    added Rezultat and Tekmovanje classes

---
+++ Rezultat.py
@@ -0,0 +1,22 @@
+class Rezultat:
+    def __init__(self, swim, division, run, name, profession, country, age, runDistance, bib, state, bike, genderRank, overall, swimDistance, overallRank, points, t2, bikeDistance, t1, divRank):
+        self.swim = swim
+        self.division = division
+        self.run = run
+        self.name = name
+        self.profession = profession
+        self.country = country
+        self.age = age
+        self.runDistance = runDistance
+        self.bib = bib
+        self.state = state
+        self.bike = bike
+        self.genderRank = genderRank
+        self.overall = overall
+        self.swimDistance = swimDistance
+        self.overallRank = overallRank
+        self.points = points
+        self.t2 = t2
+        self.bikeDistance = bikeDistance
+        self.t1 = t1
+        self.divRank = divRank
---
+++ Tekmovanje.py
@@ -0,0 +1,5 @@
+class Tekmovanje:
+    def __init__(self, competition_name, year, results):
+        self.competition_name = competition_name
+        self.year = year
+        self.results = results
