82e6d041a45c14646d1b4df518c83c3e56645e9f
TjanKazar <122014731+TjanKazar@users.noreply.github.com>
2024-05-28 21:36:30 +0200

    Update ozra_api.py

--- ozra_api.py
+++ ozra_api.py
@@ -1,4 +1,4 @@
-import py2psql
+import config
 import psycopg2
 import json
 import Tekmovanje
@@ -8,14 +8,15 @@ import datetime
 import poslogika
 from flask_cors import CORS
 
-conn = psycopg2.connect(user=py2psql.username,
-                        password=py2psql.pwd,
-                        host=py2psql.hostname,
-                        port=py2psql.port_id,
-                        database=py2psql.database)
+conn = psycopg2.connect(user=config.username,
+                        password=config.pwd,
+                        host=config.hostname,
+                        port=config.port_id,
+                        database=config.database)
 cur = conn.cursor()
 
 app = Flask("ozraAPI")
+
 CORS(app)
 
 #NAMIZNA ADMINISTRATORSKA APLIKACIJA
@@ -32,56 +33,59 @@ def put_tekmovanje():
         conn.commit()
         return jsonify({"message": "Result changed successfully"}), 200
 
-@app.put('/Rezultatput')
+@app.put("/rezultatput")
 def put_rezultat():
-        data = request.get_json()
-        id = data.get('id')
-        swim = data.get('swim')
-        division = data.get('division')
-        run = data.get('run')
-        name = data.get('name')
-        profession = data.get('profession')
-        country = data.get('country')
-        age = data.get('age')
-        run_distance = data.get('run_distance')
-        bib = data.get('bib')
-        state = data.get('state')
-        bike = data.get('bike')
-        gender_rank = data.get('gender_rank')
-        overall = data.get('overall')
-        swim_distance = data.get('swim_distance')
-        overall_rank = data.get('overall_rank')
-        points = data.get('points')
-        t2 = data.get('t2')
-        bike_distance = data.get('bike_distance')
-        t1 = data.get('t1')
-        div_rank = data.get('div_rank')
+    data = request.get_json()
+    id = data.get('id')
+    swim = data.get('swim')
+    division = data.get('division')
+    run = data.get('run')
+    name = data.get('name')
+    profession = data.get('profession')
+    country = data.get('country')
+    age = data.get('age')
+    run_distance = data.get('run_distance')
+    bib = data.get('bib')
+    state = data.get('state')
+    bike = data.get('bike')
+    gender_rank = data.get('gender_rank')
+    overall = data.get('overall')
+    swim_distance = data.get('swim_distance')
+    overall_rank = data.get('overall_rank')
+    points = data.get('points')
+    t2 = data.get('t2')
+    bike_distance = data.get('bike_distance')
+    t1 = data.get('t1')
+    div_rank = data.get('div_rank')
 
-        cur.execute("UPDATE rezultat SET swim = %s, division = %s, run = %s, name = %s, profession = %s, country = %s, age = %s, run_distance = %s, bib = %s, state = %s, bike = %s, gender_rank = %s, overall = %s, swim_distance = %s, overall_rank = %s, points = %s, t2 = %s, bike_distance = %s, t1 = %s, div_rank = %s WHERE id = %s", (swim, division, run, name, profession, country, age, run_distance, bib, state, bike, gender_rank, overall, swim_distance, overall_rank, points, t2, bike_distance, t1, div_rank, id))
+    cur.execute("""
+        UPDATE Rezultat SET swim = %s, division = %s, run = %s, name = %s, profession = %s, country = %s,
+            age = %s, run_distance = %s, bib = %s, state = %s, bike = %s, gender_rank = %s, overall = %s,
+            swim_distance = %s, overall_rank = %s, points = %s, t2 = %s, bike_distance = %s, t1 = %s, div_rank = %s
+        WHERE id = %s
+    """, (swim, division, run, name, profession, country, age, run_distance, bib, state, bike, gender_rank,
+          overall, swim_distance, overall_rank, points, t2, bike_distance, t1, div_rank, id))
 
-        conn.commit()
-        return jsonify({"message": "Result changed successfully"}), 200
+    conn.commit()
+
+    return jsonify({"message": "Result updated successfully"}), 200
 
 #desktop 2
-@app.delete('/tekmovanjedelete')
-def delete_tekmovanje():
-        data = request.get_json()
-        id = data.get('id')
+@app.delete('/tekmovanjedelete/<int:id>')
+def delete_tekmovanje(id):
         cur.execute("DELETE FROM tekmovanje WHERE id = %s", (id,))
         
         conn.commit()
 
         return jsonify({"message": "Result deleted successfully"}), 204  
   
-@app.delete('/rezultatdelete')
-def delete_rezultat():
-        data = request.get_json()
-        id = data.get('id')
+@app.delete('/rezultatdelete/<int:id>')
+def delete_rezultat(id):
         cur.execute("DELETE FROM rezultat WHERE id = %s", (id,))
         
         conn.commit()
 
-        return jsonify({"message": "Result deleted successfully"}), 204  
+        return jsonify({"message": "Result deleted successfully"}), 204 
 
 #desktop 3
 @app.post("/rezultatpost")
@@ -263,6 +267,9 @@ def get_tekmovanje_name(competition_name):
                 })
     return jsonify(result)
 
+
+
+
 @app.get('/porocilotekmovanja/<int:id>')
 def get_porocilo(id):
     query = "SELECT Rezultat_ID FROM rezultat_tekmovanje WHERE Tekmovanje_ID = %s"
@@ -310,7 +317,6 @@ def get_best_swimmers(name, surname):
 def get_best_runners(name, surname):
 
     full_name = name.capitalize() + " " + surname.capitalize()
-    # First, find the competitors with the given name and surname in the rezultat table
     query = """
     SELECT rezultat_tekmovanje.Rezultat_ID, rezultat.run
     FROM rezultat_tekmovanje
@@ -320,7 +326,6 @@ def get_best_runners(name, surname):
     cur.execute(query, (full_name,))
     rows = cur.fetchall()
 
-    # Assume poslogika.get_top_runners is defined to get the top runner IDs from the rows
     top_runner_ids = poslogika.get_shortest_time_run(rows)
 
     top_runners = []
@@ -401,6 +406,44 @@ def get_rezultat(id):
             "div_rank": row[20]
         }
     return jsonify(result)
+
+from flask import jsonify
+
+@app.get("/rezultat_byname/<name>/<bib>")
+def get_rezultat_byname(name, bib):
+    query = "SELECT * FROM Rezultat WHERE name = %s AND bib = %s"
+    cur.execute(query, (name, bib))
+    row = cur.fetchone()
+    
+    if row:
+        result = {
+            "id": row[0],
+            "swim": row[1],
+            "division": row[2],
+            "run": row[3],
+            "name": row[4],
+            "profession": row[5],
+            "country": row[6],
+            "age": row[7],
+            "run_distance": row[8],
+            "bib": row[9],
+            "state": row[10],
+            "bike": row[11],
+            "gender_rank": row[12],
+            "overall": row[13],
+            "swim_distance": row[14],
+            "overall_rank": row[15],
+            "points": row[16],
+            "t2": row[17],
+            "bike_distance": row[18],
+            "t1": row[19],
+            "div_rank": row[20]
+        }
+        conn.commit()  # Commit the changes to the database
+        return jsonify(result), 200
+    else:
+        return jsonify({"message": "Result not found"}), 404
+
 
 
 @app.get('/tekmovalec/<name>')
@@ -725,7 +768,6 @@ def objava_delete(id):
      
 @app.get('/uporabnik/<string:name>/<string:password>')
 def get_uporabnik(name, password):
-    # Query to find the first user with the given name and password
     cur.execute("SELECT * FROM uporabnik WHERE name = %s AND password = %s LIMIT 1", (name, password))
     user = cur.fetchone()
 
@@ -756,6 +798,9 @@ def obvestila_get(id):
               "body": row[3]
          })
     return result
+
+
+
 
 if __name__ == '__main__':
     app.run()
